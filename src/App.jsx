7f689333a91b13668573bf58dc57cd13f4a41c7a
reynaldomarchell <reynaldomarchellba@gmail.com>
2023-12-15 16:09:54 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="flex">
+        {/* Side Navigation Bar */}
+        <Navbar />
+        {/* Main component on basis of selected navigation from nav bar */}
+        <main className="grow">
+          <Dashboard />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
