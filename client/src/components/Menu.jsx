0334d9f15c96219bc0a35532de73e20c3889e9a1
reynaldomarchell <reynaldomarchellba@gmail.com>
2023-12-17 09:18:43 +0700

    feat: create menu table details component

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,7 +1,13 @@
+import MenuTableDetail from "./MenuTableDetail";
+
 function Menu() {
   return (
     <div className="flex flex-col py-10 px-16 h-screen overflow-y-auto w-full gap-2">
       <h2 className="text-2xl">Menu</h2>
+      <div>
+        <h3 className="py-4 text-lg">Edit Menu</h3>
+        <MenuTableDetail />
+      </div>
     </div>
   );
 }
---
+++ client/src/data/menuData.js
@@ -0,0 +1,68 @@
+
+const columns = [
+  { key: "id", label: "ID", sortable: true },
+  { key: "name", label: "NAME", sortable: true },
+  { key: "type", label: "TYPE", sortable: true },
+  {
+    key: "price",
+    label: "PRICE",
+    sortable: true,
+  },
+  { key: "status", label: "STATUS", sortable: true },
+  { key: "actions", label: "ACTIONS" },
+
+];
+
+const statusOptions = [
+  { name: "Available", key: "available" },
+  { name: "Empty", key: "empty" },
+  { name: "limited", key: "limited" },
+
+];
+
+const menu = [
+  {
+    id: "1",
+    name: "Nasi Goreng",
+    type: "Food",
+    price: 15000,
+    status: "available",
+  },
+  {
+    id: "2",
+    name: "Es Teh",
+    type: "Drink",
+    price: 5000,
+    status: "available",
+  },
+  {
+    id: "3",
+    name: "Ayam Goreng",
+    type: "Food",
+    price: 20000,
+    status: "limited",
+  },
+  {
+    id: "4",
+    name: "Ayam Bakar",
+    type: "Food",
+    price: 20000,
+    status: "empty",
+  },
+  {
+    id: "5",
+    name: "Bubur Ayam",
+    type: "Food",
+    price: 20000,
+    status: "available",
+  },
+  {
+    id: "6",
+    name: "Es Jeruk",
+    type: "Drink",
+    price: 5000,
+    status: "available",
+  },
+];
+
+export { columns, menu, statusOptions };
